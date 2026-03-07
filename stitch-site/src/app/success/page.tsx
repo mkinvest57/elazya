aa0e279800023496c384782b488b4d8e7141023e
stealthstocks <nanashinfinite@gmail.com>
2026-03-07 03:45:36 +0100

    fix(website): Update DMG download date to March 7th

--- stitch-site/src/app/success/page.tsx
+++ stitch-site/src/app/success/page.tsx
@@ -176,7 +176,7 @@ function SuccessContent() {
                             >
                                 <Download className="w-5 h-5" />
                                 Télécharger le DMG
-                                <span className="text-sm font-normal opacity-80 ml-1">~184 MB (Mis à jour le 3 Mars 2026 à 21:44)</span>
+                                <span className="text-sm font-normal opacity-80 ml-1">~184 MB (Mis à jour le 7 Mars 2026 à 03:00)</span>
                             </a>
 
                             {/* Feature badges */}
